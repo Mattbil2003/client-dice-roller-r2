92ba18dc8bfc09c1fb62d592ccec39915fc9d86e
Mattbil2003 <90293237+Mattbil2003@users.noreply.github.com>
2024-02-25 23:35:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
       return count
     }
   function rollDice(count) {
-    fetch(`drserver.azurewebsites.net/roll?count=${count}`)
+    fetch(`https://drserver.azurewebsites.net/?count=${count}`)
     .then(response => {
       if (!response.ok) {
           throw new Error('Network response was not ok');
